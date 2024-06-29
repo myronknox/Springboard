dabcbd55cb72c048b495b15c6a729b559d3c36fa
myronknox <knoxmyron@gmail.com>
2024-06-29 18:20:34 -0400

    yes

---
+++ Caesar.js
@@ -0,0 +1,7 @@
+let locationParty = "GARDEN";//String
+let isPartySafe = "false";//Boolean//String
+let messageEncrypted = "";//String
+let messageDecrypted = "";//String
+
+const shiftValue = 3;//Should never change
+Number.isInteger(shiftValue);
