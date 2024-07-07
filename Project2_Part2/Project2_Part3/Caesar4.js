8724d5fd2d3bb4094fe17be36de8b3b9205afec9
myronknox <knoxmyron@gmail.com>
2024-07-07 15:33:04 -0400

    done

---
+++ Project2_Part2/Project2_Part3/Caesar4.js
@@ -0,0 +1,53 @@
+const emblemClue1 = "Eagle";
+const emblemClue2 = "Laurel";
+const emblemClue3 = 7;
+
+
+//let locationStart = "";
+//if (emblemClue1 === "Eagle")
+//  {
+//      locationStart = "Forum";//
+//  }
+//else if (emblemClue1 === "Lion")    
+//  {
+//      locationStart = "Colosseum";
+//  }
+
+//else
+//   {
+    //  locationStart = "Villa";
+//   }// STEP 1
+
+
+
+//if (emblemClue2 === "Laurel" && locationStart === "Forum")
+
+//    {
+    //locationStart += " of Augustus";
+ // }
+  //else if (emblemClue2 === "Grapes" || locationStart === "Villa")
+  //{
+    //locationStart += " of Pompey";
+  //} STEP 2
+
+
+  //switch (emblemClue3)
+//{
+  //case 7:
+  //  locationStart += " North";
+    //break;
+  //case 3:
+   // locationStart += " South";
+   // break;
+  //case 9:
+    //locationStart += " East";
+    //break;
+ // case 4:
+    //locationStart += " West";
+   // break;
+//} STEP 3
+
+//Double equals converts the operands to the same type.  Triple equals is more strict and converts the operands to the same type and value.//
+  
+
+
