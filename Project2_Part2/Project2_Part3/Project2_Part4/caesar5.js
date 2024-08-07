95414120eb9ecad8a712c3e2b798d59797a5d219
myronknox <knoxmyron@gmail.com>
2024-08-07 14:45:24 -0400

    done

---
+++ Project2_Part2/Project2_Part3/Project2_Part4/caesar5.js
@@ -0,0 +1,21 @@
+const guests = ["ANTHONY", "CICERO", "CASSIUS", "CLEOPATRA"];
+
+//guests.unshift("BRUTUS"); STEP 1 
+
+//By using the array guests [0], you can check the first element-QUESTION 1
+
+//guests.push("AUGUSTUS, "LUCIA"); STEP 2
+
+//const spartacusIndex = guests.indexOf("SPARTACUS"); STEP 3
+
+//-1-Question 2
+
+//const indexToRemove = guests.indexOf("CASSIUS");
+//guests.splice(indexToRemove, 1);-STEP 4
+
+//const specialGuests = guests.slice(0, 3);-STEP 5
+
+//const honoredGuests = guests.slice(0, 1);
+//const otherGuests = guests.slice(1); 
+//otherGuests.sort(); // Sorts the other guests.
+//const sortedGuests = honoredGuests.concat(otherGuests);
