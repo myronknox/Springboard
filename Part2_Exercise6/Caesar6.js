9de47ef668e5ee888f9b9ee9647bf74bf65ac935
myronknox <knoxmyron@gmail.com>
2024-08-11 15:07:22 -0400

    finished

---
+++ Part2_Exercise6/Caesar6.js
@@ -0,0 +1,35 @@
+const guests = {
+    ANTONY: {
+      title: "General",
+      region: "Rome",
+      dietaryPreference: "Vegetarian",
+      pastGifts: ["Golden Laurel", "Chariot"]
+    },
+    CICERO: {
+      title: "Orator",
+      region: "Arpinum",
+      dietaryPreference: "Omnivore",
+      pastGifts: ["Scroll of Proverbs", "Quill"]
+    }
+  };
+
+  //guests.BRUTUS = {
+    //title: "Senator",
+    //region: "Rome",
+    //dietaryPreference: "Vegan",
+    //pastGifts: ["Silver Dagger", "Marble Bust"]
+ // }; Step 1
+
+ //guests.CICERO.pastGifts.push("Golden Lyre"); Step 2
+
+ //const antonyRegion = guests.ANTONY.region; Step 3
+
+ //delete guests.CICERO; Step 4
+
+ // const generalProfile = guests.ANTONY;
+ //generalProfile.region = "Egypt"; Step 5 
+
+ //It will be Egypt.  When you make a change to one variable it makes a change in other variables QUESTION 1
+
+ 
+  
