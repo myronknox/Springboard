1750e94f1e38f29539af5f9a38916fb4f25c84fe
myronknox <knoxmyron@gmail.com>
2024-08-12 15:52:37 -0400

    finished

---
+++ Part2_Exercise6/Part2_Exercise7/Caesar7.js
@@ -0,0 +1,18 @@
+const friend = "BRUTUS"
+const shiftValue = 3;
+
+//const alphabet = "abcdefghijklmnopqrstuvwxyz"; Step 1
+
+//let encryptedName = "";
+
+//for (let i = 0; i < friend.length; i++)
+    //{
+    //  const currentLetter = friend[i];
+      //const currentIndex = alphabet.indexOf(currentLetter.toLowerCase());
+     // const newIndex = (currentIndex + shiftValue) % alphabet.length;
+     // encryptedName += alphabet[newIndex].toUpperCase();
+   // } Step 2
+
+   //It gets rid of redundant code.  It allows for consistent encryption. QUESTION 1 
+
+   //The modulus operator in this example brings the encryption back to A when it passes Z. QUESTION 2
